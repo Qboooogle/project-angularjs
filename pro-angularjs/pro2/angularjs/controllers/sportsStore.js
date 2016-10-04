da176cf1d360369fee7dc275da67df2dd59e39a2
Qboooogle <1432500814@qq.com>
2016-10-04 21:39:27 +0800

    创建运动商店项目

---
+++ pro-angularjs/pro2/angularjs/controllers/sportsStore.js
@@ -0,0 +1,23 @@
+﻿angular.module("sportsStore")
+.controller("sportsStoreCtrl", function ($scope) {
+
+    $scope.data = {
+        products: [
+            {
+                name: "产品 #1", description: "一件运动产品",
+                category: "类型 #1", price: 100
+            },
+            {
+                name: "产品 #2", description: "一件运动产品",
+                category: "类型 #1", price: 110
+            },
+            {
+                name: "产品 #3", description: "一件运动产品",
+                category: "类型 #2", price: 210
+            },
+            {
+                name: "产品 #4", description: "一件运动产品",
+                category: "类型 #3", price: 202
+            }]
+    };
+});
