9d0d7cf27794b126fd05881a9864d2f701cd9ff9
Qboooogle <1432500814@qq.com>
2016-10-04 15:22:44 +0800

    环境搭建成功

--- pro-angularjs/pro1/server.js
+++ pro-angularjs/pro1/server.js
@@ -1,6 +1,12 @@
-var connect = require('connect');
+var connect = require('connect'),
+    serveStatic = require('serve-static');
 
-connect.createServer(
-    connect.static("./angularjs")
+var app = connect();
+app.use(serveStatic("./angularjs"));
+app.listen(8000);
 
-).listen(5000);
+// connect.createServer(
+//     connect.static("./angularjs")
+
+// ).listen(5000);
+console.log("服务器已经在8000端口运行。。。")
