473bde74c45ce78719f14e6ad277271f307cdffb
Qboooogle <1432500814@qq.com>
2016-10-05 17:41:24 +0800

    完成下单，并将订单提交至数据库

--- pro-angularjs/pro2/angularjs/controllers/sportsStore.js
+++ pro-angularjs/pro2/angularjs/controllers/sportsStore.js
@@ -1,14 +1,32 @@
 ﻿angular.module("sportsStore")
 .constant("dataUrl", "http://localhost:5000/products")
-.controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+.constant("orderUrl", "http://localhost:5000/order")
+.controller("sportsStoreCtrl", function ($scope, $http, $location, dataUrl, orderUrl, cart) {
 
     $scope.data = {};
 
     $http.get(dataUrl)
-    .success(function(data) {
-        $scope.data.products = data;
-    })
-    .error(function(error) {
-        $scope.data.error = error;
-    });
+	    .success(function(data) {
+	        $scope.data.products = data;
+	    })
+	    .error(function(error) {
+	        $scope.data.error = error;
+	    });
+
+	$scope.sendOrder = function(shippingDetails) {
+		var order = angular.copy(shippingDetails);
+		order.products = cart.getProducts();
+
+		$http.post(orderUrl, order)
+		    .success(function(data) {
+		    	$scope.data.orderId = data.id;
+		    	cart.getProducts().length = 0;
+		    })
+		    .error(function(error) {
+                $scope.data.orderError = error;
+		    })
+		    .finally(function() {
+		    	$location.path("/complete");
+		    });
+	}
 });
