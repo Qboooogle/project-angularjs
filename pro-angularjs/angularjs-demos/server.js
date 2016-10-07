3015d64f5e5f6e37475052b360b0337240a773a8
Qboooogle <1432500814@qq.com>
2016-10-07 16:34:47 +0800

    demo-13使用控制器和作域

--- pro-angularjs/angularjs-demos/server.js
+++ pro-angularjs/angularjs-demos/server.js
@@ -2,14 +2,14 @@
 * @Author: QBoooogle
 * @Date:   2016-10-04 19:16:18
 * @Last Modified by:   QBoooogle
-* @Last Modified time: 2016-10-07 13:17:46
+* @Last Modified time: 2016-10-07 16:15:13
 */
 
 var connect = require('connect'),
     serveStatic = require('serve-static');
 
 var app = connect();
-app.use(serveStatic("./demo-12"));
+app.use(serveStatic("./demo-13"));
 app.listen(8000);
 console.log("服务器已经在8000端口运行。通过http://127.0.0.1:8000/test.html访问");
 
