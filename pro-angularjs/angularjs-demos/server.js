d2fc78ae4e0d208b5475f62f4e571f2d8fb8b6e0
Qboooogle <1432500814@qq.com>
2016-10-07 13:21:44 +0800

    demo-12(表单数据绑定）

--- pro-angularjs/angularjs-demos/server.js
+++ pro-angularjs/angularjs-demos/server.js
@@ -1,15 +1,15 @@
 /*
 * @Author: QBoooogle
 * @Date:   2016-10-04 19:16:18
-* @Last Modified by:   Qboooogle
-* @Last Modified time: 2016-10-06 16:42:00
+* @Last Modified by:   QBoooogle
+* @Last Modified time: 2016-10-07 13:17:46
 */
 
 var connect = require('connect'),
     serveStatic = require('serve-static');
 
 var app = connect();
-app.use(serveStatic("./demo-11"));
+app.use(serveStatic("./demo-12"));
 app.listen(8000);
 console.log("服务器已经在8000端口运行。通过http://127.0.0.1:8000/test.html访问");
 
